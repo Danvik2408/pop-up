2ed62970e056a11e34bfad4c344f825e35ad83cf
Danvik2408 <DanKaftan68@gmail.com>
2023-08-24 15:26:06 +0300

    reset style, add background

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ support.onclick=function(){
     popUp.style.display='flex'
     support.style.display='none'
 }
+
+cross.onclick=function(){
+    popUp.style.display='none'
+    support.style.display='flex'
+}
